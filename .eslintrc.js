603e1226919bcf1615ac3ef1c397fbdf37b5c178
Benjamin Irvin <ben@innerdvations.com>
2021-03-13 18:57:03 +0100

    sort keys

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,38 @@
 module.exports = {
-  overrides: [
-    {
-      files: ['tests/**/*.ts', 'tests/**/*.tsx'],
-      env: {
-        mocha: true,
-      },
+  env: {
+    browser: false,
+    es2021: true,
+  },
+  extends: ['airbnb/base'],
+  ignorePatterns: ['**/dist/*.js'],
+  overrides: [{
+    env: {
+      mocha: true,
     },
-  ],
+    files: ['tests/**/*.ts', 'tests/**/*.tsx'],
+  }],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  // parserOptions: {
+  //   ecmaVersion: 12,
+  //   sourceType: 'module',
+  // },
+  rules: {
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+    }],
+    'linebreak-style': ['error', 'unix'],
+    'no-underscore-dangle': 'off', // better than the alternatives in this simple project
+
+  },
   settings: {
     'import/resolver': {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  env: {
-    browser: false,
-    es2021: true,
-  },
-  extends: ['airbnb/base'],
-  // parserOptions: {
-  //   ecmaVersion: 12,
-  //   sourceType: 'module',
-  // },
-  rules: {
-    'linebreak-style': ['error', 'unix'],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'no-underscore-dangle': 'off', // better than the alternatives in this simple project
   },
 };
